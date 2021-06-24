812fc298f9d08940dfa966226a650a1b1beb6ed1
José Ricardo David <david_Ricardo@live.com>
2021-06-24 19:38:33 -0300

    Add project files.

---
+++ FluentValidation/ModelsValidator/BeerValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentValidation.ModelsValidator
+{
+    public class BeerValidator : AbstractValidator<Beer>
+    {
+        public BeerValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().NotNull().MinimumLength(3).WithMessage("Nome inválido");
+            RuleFor(x => x.Alcohol).NotNull().WithMessage("Valor obrigatório").GreaterThan(0).WithMessage("Valor tem que ser maior que zero.");
+            RuleFor(x => x.Brand).NotNull().WithMessage("Campo não pode ser nulo.").MaximumLength(20).NotEqual("Corona").WithMessage("Não pode ser igual a Corona!");
+        }
+
+
+    }
+}
---
+++ FluentValidation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation.Models;
+using FluentValidation.ModelsValidator;
+using FluentValidation.Results;
+
+namespace FluentValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var beer = new Beer()
+            {
+                Name = "Tremens",
+                Alcohol = 0,
+                Brand = "Corona"
+            };
+
+            var validator = new BeerValidator();
+            ValidationResult result = validator.Validate(beer);
+
+            if (!result.IsValid)
+            {
+                result.Errors.ForEach(x => Console.WriteLine(x.ErrorMessage));
+            }
+            else Console.WriteLine("Ok!");
+
+
+            Console.ReadLine();
+        }
+    }
+}
